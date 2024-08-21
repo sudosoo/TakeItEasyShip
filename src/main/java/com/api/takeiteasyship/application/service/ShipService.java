6d38e3d7d184900ea894e42e3e057b3015a662f2
antidote <ljh8651@nate.com>
2024-08-21 18:24:44 +0900

    [feat] 캐시 설정 추가

--- src/main/java/com/api/takeiteasyship/application/service/ShipService.java
+++ src/main/java/com/api/takeiteasyship/application/service/ShipService.java
@@ -2,11 +2,14 @@ package com.api.takeiteasyship.application.service;
 
 import com.api.takeiteasyship.application.dto.request.ShippingCancelRequestDto;
 import com.api.takeiteasyship.application.dto.request.ShippingRequestDto;
+import com.api.takeiteasyship.application.dto.response.ShippingDefaultResp;
 import com.api.takeiteasyship.domain.entity.Ship;
 import com.api.takeiteasyship.domain.repository.ShipRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -28,10 +31,10 @@ public class ShipService {
                 .ifPresent(repository::delete);
         log.info("Ship canceled: {}", req.getOrderId());
     }
-
-    public RespShip get(ShippingCancelRequestDto req){
-        repository.findByOrderId(UUID.fromString(req.getOrderId()))
-                .ifPresent(repository::delete);
+    @CachePut(value = "shippingEntity", key = "#id")
+    public ShippingDefaultResp get(String id){
+        var ship = repository.findByOrderId(UUID.fromString(id))
+                .orElseThrow(()-> new IllegalArgumentException("Ship not found"));
 
     }
 
---
+++ src/main/java/com/api/takeiteasyship/presentation/ShipController.java
@@ -0,0 +1,14 @@
+package com.api.takeiteasyship.presentation;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ShipController {
+
+    public void register() {
+
+    }
+
+}
